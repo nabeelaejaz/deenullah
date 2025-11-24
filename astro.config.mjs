1f4ebff5ac15073551351cfc664a5a31bffd52da
Nabeel Aejaz <nabeelaejaz@gmail.com>
2025-11-24 08:01:32 +0500

    Added Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import solidJs from '@astrojs/solid-js';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), db()],
+  site: 'https://www.deenullah.com',
+  integrations: [solidJs(), db(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
