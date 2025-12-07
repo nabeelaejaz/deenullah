1f791fb72745022516e174373b885a9380b76c23
Nabeel Aejaz <nabeelaejaz@gmail.com>
2025-12-07 06:32:56 +0500

    Added Partytown and Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
 import solidJs from '@astrojs/solid-js';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.deenullah.com',
-  integrations: [solidJs(), db(), sitemap()],
+  integrations: [solidJs(), db(), sitemap(),
 
+  partytown({
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   },
